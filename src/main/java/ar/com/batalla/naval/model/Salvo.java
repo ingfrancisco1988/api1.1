196ba11731adb416337ae8368ffa524ad90aeb57
francisco <francisco.javier1988@gmail.com>
2019-10-06 20:45:50 -0300

    agregando cambios de salvo

--- src/main/java/ar/com/batalla/naval/model/Salvo.java
+++ src/main/java/ar/com/batalla/naval/model/Salvo.java
@@ -1,5 +1,69 @@
 package ar.com.batalla.naval.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Salvo {
+	
+	@Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private int turn;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="game_player")
+    private GamePlayer gamePlayer;
+    
+    @ElementCollection
+    @Column(name="salvoes_locations")
+    private List<String> locations;
+
+    public Salvo() {}
+
+    public Salvo(GamePlayer gamePlayer, int turn, List<String> locations){
+        this.gamePlayer = gamePlayer;
+        this.turn = turn;
+        this.locations = locations;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getTurn() {
+    	return turn;
+    }
+
+    public GamePlayer getGamePlayer() {
+    	return gamePlayer;
+    }
+
+    public void setGamePlayer(GamePlayer gamePlayer) {
+        this.gamePlayer = gamePlayer;
+    }
+
+    public List<String> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<String> locations) {
+        this.locations = locations;
+    }
+
+    public int getNumberOfShots(){
+        int number = locations.size();
+        return number;
+    }
 
 }
--- src/main/java/ar/com/batalla/naval/repository/SalvoRepository.java
+++ src/main/java/ar/com/batalla/naval/repository/SalvoRepository.java
@@ -1,5 +1,11 @@
 package ar.com.batalla.naval.repository;
 
-public interface SalvoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ar.com.batalla.naval.model.Salvo;
+
+public interface SalvoRepository extends JpaRepository<Salvo,Long>{
+	
+Salvo findById(long id);
 
 }
